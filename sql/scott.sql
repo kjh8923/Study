cddd14a4019b1e93ce828eaced8058ce4a9268cd
kjh8923 <96833967+kjh8923@users.noreply.github.com>
2022-02-22 17:37:11 +0900

    Add files via upload

--- sql/scott.sql
+++ sql/scott.sql
@@ -19,3 +19,18 @@ COMMIT;
 INSERT INTO mvc_board(BID, BNAME, BTITLE, BCONTENT, BHIT, BGROUP, BSTEP, BINDENT) VALUES(MVC_BOARD_SEQ.NEXTVAL, 'KJH', '게시판을 만들어보아요', '재미있는 게시판 만들기 놀이~~~!', 0, MVC_BOARD_SEQ.CURRVAL, 0, 0);
 
 COMMIT;
+
+create table ticket(
+consumerId varchar2(20),
+countnum number(4),
+constraint countnum_check check (countnum between 1 and 4)
+);
+
+commit;
+
+create table card(
+consumerId varchar2(20),
+amount number(4)
+);
+
+commit;
